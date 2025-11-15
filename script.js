35273e05bb621f7a685a2dd144c6695342a40649
Lana Nakai <koalanako@gmail.com>
2025-11-15 11:44:18 -0800

    tutorials

---
+++ script.js
@@ -0,0 +1,13 @@
+let name="nugget";
+console.log("hello" + name);
+
+function say_hello(name) {
+    console.log("hello + name")
+}
+
+say_hello("user")
+
+function increment() {
+	score_element = document.getElementById("score");
+	score_element.innerHTML = parseInt(score_element.innerHTML) + 1;
+}
